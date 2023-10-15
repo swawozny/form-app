346820d0d11729d1ab6e13c2df1691bcd51f1184
swawozny <sebastianwawony@wp.pl>
2023-10-15 23:25:24 +0200

    Add header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
+import React from "react";
+import {Layout, theme} from "antd";
+
 import MainForm from "./containers/MainForm";
 
+const {Header, Content, Footer} = Layout;
+
 const App = () => {
+    const {token: {colorBgContainer, colorInfo}} = theme.useToken();
+
   return (
-      <MainForm/>
+      <Layout>
+          <Header style={{backgroundColor: colorInfo}}>
+              <h1 style={{color: colorBgContainer}}>FormAPP</h1>
+          </Header>
+          <Content style={{padding: '0 50px'}}>
+              <div style={{padding: 24, minHeight: 380, background: colorBgContainer}}>
+                  <MainForm/>
+              </div>
+          </Content>
+          <Footer/>
+      </Layout>
   );
 }
 
